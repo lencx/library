5fef8faac666d208eec3742b8718cfd38bf30048
Len <cxin1314@gmail.com>
2017-04-22 16:47:03 +0800

    add method#drawDashLine

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ new Carousel({
     indexStyle: 'disc' // disc | number
 })
 ```
+
+## Utility functions for the canvas package
+
+```js
+let $ = new CanvasApp('#myCanvas')
+$.drawRect(x, y, width, height, color, isFill, isClear)
+$.drawArc(x, y, radius, startDeg, endDeg, color, startPoint, anticlockwise, isFill, isOnlyArc)
+$.drawSector(x, y, radius, startDeg, endDeg, color, startPoint, anticlockwise, isFill)
+$.drawRoundRect(x, y, width, height, radius, color, isFill, tr, br, bl, tl)
+$.drawSolidLine(startX, startY, endX, endY, lineWidth, color)
+$.drawDashLine(startX, startY, endX, endY, color, setLineDash, lineWidth, lineCap)
+```
--- api/canvas.api.js
+++ api/canvas.api.js
@@ -4,6 +4,15 @@
  * @license Apache-2.0
  */
 
+'use strict';
+
+function CanvasApp(selector) {
+    this.canvas = document.querySelector(selector)
+    this.ctx = this.canvas.getContext('2d')
+}
+// Usage:
+// let $ = new CanvasApp('#myCanvas')
+
 /**
  * Draw Rect
  * [filled rectangle | rectangle outline | clears the specified rectangle area]
@@ -14,22 +23,22 @@
  * @param {String} [color | clear]
  * @param {Boolean|Number} isFill | lineWidth - default value: `false` [not filled]
  * @example
- * => drawRect(200, 200, 100, 100, 'red') // red rectangle is not filled
- * => drawRect(200, 400, 100, 100, 'green', true) // green rectangle is filled
- * => drawRect(0, 0, 500, 500, 'clear') // clears the specified rectangle area
- * => drawRect(200, 200, 200, 100, 'red', 10) // not filled and line width is `10`
+ * => $.drawRect(200, 200, 100, 100, 'red') // red rectangle is not filled
+ * => $.drawRect(200, 400, 100, 100, 'green', true) // green rectangle is filled
+ * => $.drawRect(0, 0, 500, 500, 'clear') // clears the specified rectangle area
+ * => $.drawRect(200, 200, 200, 100, 'red', 10) // not filled and line width is `10`
  */
-function drawRect(x, y, width, height, color, isFill, isClear) {
+CanvasApp.prototype.drawRect = function(x, y, width, height, color, isFill, isClear) {
     if(color === 'clear') {
-        ctx.clearRect(x, y, width, height)
+        this.ctx.clearRect(x, y, width, height)
     } else {
         if(isFill === true) {
-            ctx.fillStyle = color
-            ctx.fillRect(x, y, width, height)
+            this.ctx.fillStyle = color
+            this.ctx.fillRect(x, y, width, height)
         } else {
-            if(typeof isFill === 'number') ctx.lineWidth = isFill
-            ctx.strokeStyle = color
-            ctx.strokeRect(x, y, width, height)
+            if(typeof isFill === 'number') this.ctx.lineWidth = isFill
+            this.ctx.strokeStyle = color
+            this.ctx.strokeRect(x, y, width, height)
         }
     }
 }
@@ -47,26 +56,26 @@ function drawRect(x, y, width, height, color, isFill, isClear) {
  * @param {Boolean|Number} isFill | lineWidth - default value: `false` [not filled]
  * @param {Boolean} isOnlyArc - default value: `false` [There are connections, the closed arc]
  * @example
- * => drawArc(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)')
- * => drawArc(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 'top')
- * => drawArc(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 'bottom', true)
- * => drawArc(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 80, true, true, false)
- * => drawArc(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 80, true, 10, false) // not filled and line widtth is `10`
+ * => $.drawArc(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)')
+ * => $.drawArc(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 'top')
+ * => $.drawArc(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 'bottom', true)
+ * => $.drawArc(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 80, true, true, false)
+ * => $.drawArc(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 80, true, 10, false) // not filled and line widtth is `10`
  */
-function drawArc(x, y, radius, startDeg, endDeg, color, startPoint, anticlockwise, isFill, isOnlyArc) {
+CanvasApp.prototype.drawArc = function(x, y, radius, startDeg, endDeg, color, startPoint, anticlockwise, isFill, isOnlyArc) {
     if(isFill === true) {
-        ctx.fillStyle = color
-        ctx.beginPath()
-        arcStartPoint(x, y, radius, startDeg, endDeg, startPoint, anticlockwise)
-        ctx.closePath()
-        ctx.fill()
+        this.ctx.fillStyle = color
+        this.ctx.beginPath()
+        this.arcStartPoint(x, y, radius, startDeg, endDeg, startPoint, anticlockwise)
+        this.ctx.closePath()
+        this.ctx.fill()
     } else {
-        if(typeof isFill === 'number') ctx.lineWidth = isFill
-        ctx.strokeStyle = color
-        ctx.beginPath()
-        arcStartPoint(x, y, radius, startDeg, endDeg, startPoint, anticlockwise)
-        !isOnlyArc ? ctx.closePath() : ''
-        ctx.stroke()
+        if(typeof isFill === 'number') this.ctx.lineWidth = isFill
+        this.ctx.strokeStyle = color
+        this.ctx.beginPath()
+        this.arcStartPoint(x, y, radius, startDeg, endDeg, startPoint, anticlockwise)
+        !isOnlyArc ? this.ctx.closePath() : ''
+        this.ctx.stroke()
     }
 }
 
@@ -82,25 +91,25 @@ function drawArc(x, y, radius, startDeg, endDeg, color, startPoint, anticlockwis
  * @param {Boolean} anticlockwise - default value: `false` [clockwise]
  * @param {Boolean|Number} isFill | lineWidth - default value: `false` [not filled]
  * @example
- * => drawSector()
- * => drawSector(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)')
- * => drawSector(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 'top')
- * => drawSector(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 'bottom', true)
- * => drawSector(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 80, true, true)
- * => drawSector(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 80, true, 5) // not filled and line width is `5`
+ * => $.drawSector()
+ * => $.drawSector(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)')
+ * => $.drawSector(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 'top')
+ * => $.drawSector(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 'bottom', true)
+ * => $.drawSector(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 80, true, true)
+ * => $.drawSector(200, 200, 100, 0, 80, 'rgba(0, 100, 200, .5)', 80, true, 5) // not filled and line width is `5`
  */
-function drawSector(x, y, radius, startDeg, endDeg, color, startPoint, anticlockwise, isFill) {
-    ctx.beginPath()
-    ctx.moveTo(x, y)
-    arcStartPoint(x, y, radius, startDeg, endDeg, startPoint, anticlockwise)
-    ctx.closePath()
+CanvasApp.prototype.drawSector = function(x, y, radius, startDeg, endDeg, color, startPoint, anticlockwise, isFill) {
+    this.ctx.beginPath()
+    this.ctx.moveTo(x, y)
+    this.arcStartPoint(x, y, radius, startDeg, endDeg, startPoint, anticlockwise)
+    this.ctx.closePath()
     if(isFill === true) {
-        ctx.fillStyle = color
-        ctx.fill()
+        this.ctx.fillStyle = color
+        this.ctx.fill()
     } else {
-        if(typeof isFill === 'number') ctx.lineWidth = isFill
-        ctx.strokeStyle = color
-        ctx.stroke()
+        if(typeof isFill === 'number') this.ctx.lineWidth = isFill
+        this.ctx.strokeStyle = color
+        this.ctx.stroke()
     }
 }
 
@@ -118,30 +127,30 @@ function drawSector(x, y, radius, startDeg, endDeg, color, startPoint, anticlock
  * @param {String|Number} bl [bottom-left] - as with the above usage
  * @param {String|Number} tl [top-left] - as with the above usage
  * @example
- * => drawRoundRect(200, 200, 100, 200, 10, '#00f', true, null, 2, undefined, 4) // top-right and bottom-left enable rounded. `2` and `4` can be replaced with `String`
- * => drawRoundRect(200, 200, 100, 200, 10, '#00f', true)
- * => drawRoundRect(200, 200, 100, 200, 10, '#00f', 5) // not filled and line width is `5`
+ * => $.drawRoundRect(200, 200, 100, 200, 10, '#00f', true, null, 2, undefined, 4) // top-right and bottom-left enable rounded. `2` and `4` can be replaced with `String`
+ * => $.drawRoundRect(200, 200, 100, 200, 10, '#00f', true)
+ * => $.drawRoundRect(200, 200, 100, 200, 10, '#00f', 5) // not filled and line width is `5`
  */
-function drawRoundRect(x, y, width, height, radius, color, isFill, tr, br, bl, tl) {
-    ctx.beginPath()
+CanvasApp.prototype.drawRoundRect = function(x, y, width, height, radius, color, isFill, tr, br, bl, tl) {
+    this.ctx.beginPath()
     let xr = x + radius
     let xw = x + width
     let yr = y + radius
     let yw = y + width
     let yh = y + height
-    tl || tr || br || bl ? ctx.moveTo(x, y) : ctx.moveTo(xr, y)
-    tr === undefined || tr === null ? ctx.arcTo(xw, y, xw, yr, radius) : ctx.lineTo(xw, y)
-    br === undefined || br === null  ? ctx.arcTo(xw, yh, xw - radius, yh, radius) : ctx.lineTo(xw, yh)
-    bl === undefined || bl === null  ? ctx.arcTo(x, yh, x, yh - radius, radius) : ctx.lineTo(x, yh)
-    tl === undefined || tl === null  ? ctx.arcTo(x, y, xr, y, radius) : ctx.lineTo(x, y)
-    ctx.closePath()
+    tl || tr || br || bl ? this.ctx.moveTo(x, y) : this.ctx.moveTo(xr, y)
+    tr === undefined || tr === null ? this.ctx.arcTo(xw, y, xw, yr, radius) : this.ctx.lineTo(xw, y)
+    br === undefined || br === null  ? this.ctx.arcTo(xw, yh, xw - radius, yh, radius) : this.ctx.lineTo(xw, yh)
+    bl === undefined || bl === null  ? this.ctx.arcTo(x, yh, x, yh - radius, radius) : this.ctx.lineTo(x, yh)
+    tl === undefined || tl === null  ? this.ctx.arcTo(x, y, xr, y, radius) : this.ctx.lineTo(x, y)
+    this.ctx.closePath()
     if(isFill === true) {
-        ctx.fillStyle = color
-        ctx.fill()
+        this.ctx.fillStyle = color
+        this.ctx.fill()
     } else {
-        if(typeof isFill === 'number') ctx.lineWidth = isFill
-        ctx.strokeStyle = color
-        ctx.stroke()
+        if(typeof isFill === 'number') this.ctx.lineWidth = isFill
+        this.ctx.strokeStyle = color
+        this.ctx.stroke()
     }
 }
 
@@ -151,18 +160,52 @@ function drawRoundRect(x, y, width, height, radius, color, isFill, tr, br, bl, t
  * @param {Number} startY
  * @param {Number} endX
  * @param {Number} endY
- * @param {Number} lineWidth
  * @param {String} color
+ * @param {Number} lineWidth
+ * @param {String} lineCap - [butt | round | square]
+ * @example
+ * => $.drawSolidLine(100, 100, 200, 200, 'red', 5)
+ * => $.drawSolidLine(100, 100, 300, 200, 'red', 8, 'round')
  */
-function drawSolidLine(startX, startY, endX, endY, lineWidth, color) {
-    ctx.save()
-    ctx.strokeStyle = color
-    ctx.lineWidth = lineWidth
-    ctx.beginPath()
-    ctx.moveTo(startX, startY)
-    ctx.lineTo(endX, endY)
-    ctx.stroke()
-    ctx.restore() // restore to the default state
+CanvasApp.prototype.drawSolidLine = function(startX, startY, endX, endY, color, lineWidth, lineCap) {
+    this.ctx.save()
+    this.ctx.strokeStyle = color
+    lineCap ? this.ctx.lineCap = lineCap : ''
+    this.ctx.lineWidth = lineWidth
+    this.ctx.beginPath()
+    this.ctx.moveTo(startX, startY)
+    this.ctx.lineTo(endX, endY)
+    this.ctx.stroke()
+    this.ctx.restore() // restore to the default state
+}
+
+/**
+ * Draw Dash Line
+ * @param {Number} startX
+ * @param {Number} startY
+ * @param {Number} endX
+ * @param {Number} endY
+ * @param {String} color
+ * @param {Array} setLineDash - default value: [4, 8]  https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setLineDash
+ * @param {Number} lineWidth
+ * @param {String} lineCap - [butt | round | square]
+ * @example
+ * $.drawDashLine(100, 200, 600, 600, 'red', [10, 15], 5)
+ * $.drawDashLine(100, 200, 500, 600, 'green', [5, 10, 20], 3)
+ * $.drawDashLine(100, 200, 500, 600, 'green', [5, 15, 20], 5, 'round')
+ */
+CanvasApp.prototype.drawDashLine = function(startX, startY, endX, endY, color, setLineDash, lineWidth, lineCap) {
+    this.ctx.save()
+    this.ctx.lineWidth = lineWidth
+    lineCap ? this.ctx.lineCap = lineCap : ''
+    this.ctx.strokeStyle = color
+    this.ctx.beginPath()
+    setLineDash ? this.ctx.setLineDash(setLineDash) : this.ctx.setLineDash([4, 8])
+    this.ctx.moveTo(startX, startY)
+    this.ctx.lineTo(endX, endY)
+    this.ctx.closePath()
+    this.ctx.stroke()
+    this.ctx.restore()
 }
 
 /*************************************************************************************/
@@ -186,22 +229,22 @@ function getAngle(deg) {
  * @param {String|Number} startPoint - default value: `90deg` [clockwise] | String: [top | right | bottom | left] | Number: [0 - 360]
  * @param {Boolean} anticlockwise - default value: `false` [clockwise]
  */
-function arcStartPoint(x, y, radius, startDeg, endDeg, startPoint, anticlockwise) {
-    if(startPoint === undefined) ctx.arc(x, y, radius, getAngle(startDeg), getAngle(endDeg), anticlockwise)
+CanvasApp.prototype.arcStartPoint = function(x, y, radius, startDeg, endDeg, startPoint, anticlockwise) {
+    if(startPoint === undefined) this.ctx.arc(x, y, radius, getAngle(startDeg), getAngle(endDeg), anticlockwise)
     switch(startPoint) {
         case 'top':
-            ctx.arc(x, y, radius, getAngle(startDeg - 90), getAngle(endDeg - 90), anticlockwise)
+            this.ctx.arc(x, y, radius, getAngle(startDeg - 90), getAngle(endDeg - 90), anticlockwise)
             break
         case 'right':
-            ctx.arc(x, y, radius, getAngle(startDeg), getAngle(endDeg), anticlockwise)
+            this.ctx.arc(x, y, radius, getAngle(startDeg), getAngle(endDeg), anticlockwise)
             break
         case 'bottom':
-            ctx.arc(x, y, radius, getAngle(startDeg + 90), getAngle(endDeg + 90), anticlockwise)
+            this.ctx.arc(x, y, radius, getAngle(startDeg + 90), getAngle(endDeg + 90), anticlockwise)
             break
         case 'left':
-            ctx.arc(x, y, radius, getAngle(startDeg + 180), getAngle(endDeg + 180), anticlockwise)
+            this.ctx.arc(x, y, radius, getAngle(startDeg + 180), getAngle(endDeg + 180), anticlockwise)
             break
         default:
-            ctx.arc(x, y, radius, getAngle(startDeg + startPoint - 90), getAngle(endDeg + startPoint - 90), anticlockwise)
+            this.ctx.arc(x, y, radius, getAngle(startDeg + startPoint - 90), getAngle(endDeg + startPoint - 90), anticlockwise)
     }
 }
