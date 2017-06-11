5b84b05a70b5f092b6876dc276c3ca49a50916ad
Len <cxin1314@gmail.com>
2017-06-11 12:08:48 +0800

    base@ tpl & array

--- api/base.api.js
+++ api/base.api.js
@@ -1,5 +1,5 @@
 /**
- * JavaScript base
+ * JavaScript Common Methods
  * @author Lencx
  * @license Apache-2.0
  * {@link https://github.com/lencx/library/blob/master/api/base.api.js}
@@ -66,6 +66,33 @@ function createEl(createEl, selector) {
     }
 }
 
+/**
+ * The template inserts the data
+ * @param {Object} selector - HTML Element
+ * @param {Object} selector - HTML Element (Insert position)
+ * @param {Object} data - replace the template data
+ * @example
+ * => html
+ *  <template>
+ *      <span>{name}</span>
+ *      <span>{age}</span>
+ *  </template>
+ *  <p></p>
+ * 
+ * => js
+ *  tpl('template', 'p', {
+ *      name: 'Len',
+ *      age: 24
+ *  })
+ */
+function tpl(html, inserts, data) {
+    let _html = document.querySelector(html).innerHTML.replace(/\{\w+\}/g, str => {
+        let prop = str.replace(/\{|\}/g, '')
+        return data[prop] || ''
+    })
+    return document.querySelector(inserts).innerHTML = _html
+}
+
 /*******************************************************
  * CSS
  *******************************************************/
@@ -97,6 +124,75 @@ function getRgb(color) {
     } else {
         return console.log('Invalid hex, please enter a legal color value, for example: `#ffa`, `#00a`, `#CCAAbb` or `#00aaCC` etc.')
     }
+}
+
+/** Random Color
+ * @return {String}
+ */
+function randomColor() {
+    return '#'+(0+(Math.random()*(1<<24)|0).toString(16)).slice(-6)
+}
+
+/**
+ * Random Hsla
+ * @param {Number} saturation => 0 - 100
+ * @param {Number} lightness => 0 - 100
+ * @param {Number} alpha => 0 - 1
+ * @return {String} Defaults: hsla(random, 80, 60, 1)
+ * @example
+ * // randomHsla() => hsla(random, 80, 60, 1)
+ * // randomHsla({l: 70, a: .3}) => hsla(random, 80, 70, .3)
+ * // randomHsla({a: .5}) => hsla(random, 80, 60, .5)
+ */
+function randomHsla({s=80, l=60, a=1}) {
+    // return 'hsla('+Math.ceil(Math.random()*360)+', '+s+'%, '+l+'%,'+a+')'
+    return `hsla(${Math.ceil(Math.random()*360)}, ${s}%, ${l}%, ${a})`
+}
+
+/*******************************************************
+ * Array
+ *******************************************************/
+/**
+ * Unique values in an array
+ * @param {Array} arr
+ * @return {Array}
+ * @example
+ * // let arr = [1, 2, 1, 3, 2]
+ * // unique(arr) // [1, 2, 3]
+ * // unique2(arr) // [1, 2, 3]
+ */
+// ES5
+function unique(arr) {
+    return arr.filter((item, index, arr) => {
+        return arr.indexOf(item) === index
+    })
+}
+// ES6
+function unique2(arr) {
+    return [...new Set(arr)]
+}
+
+/**
+ * Array Items Count
+ * @param {Array} arr
+ * @return {Array} Array[Object: {item, n}]
+ * @example
+ * // let a = [1, 2, 3, 1, 2, 1, 6, 0]
+ * // arrCount(a)
+ */
+function arrCount(arr) {
+    let countArr = new Set()
+    function unique2(arr) {
+        return [...new Set(arr)]
+    }
+    unique2(arr).some(item => {
+        let n = 0
+        arr.some(i => {
+            if(item === i) n++
+        })
+        countArr.add({item, n})
+    })
+    return [...countArr]
 }
 
 /*******************************************************
